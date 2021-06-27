4bea62cc688e0bf45747ca473b1e6ddd3e751aee
erique2065 <kopiyoapps@yahoo.com>
2021-06-27 10:55:04 -0700

    my commits

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,16 @@
  */
 int main(void)
 {
-  char v;
-  int w;
-  long int x;
-  long long int y;
-  float z;
+char v;
+int w;
+long int x;
+long long int y;
+float z;
 
-  printf("Size of a char: %d byte(s)\n", sizeof(v));
-  printf("Size of an int: %d byte(s)\n", sizeof(w));
-  printf("Size of a long int: %d byte(s)\n", sizeof(x));
-  printf("Size of a long long int: %d byte(s)\n", sizeof(y));
-  printf("Size of a float: %d byte(s)\n", sizeof(z));
-  return (0);
+printf("Size of a char: %d byte(s)\n", sizeof(v));
+printf("Size of an int: %d byte(s)\n", sizeof(w));
+printf("Size of a long int: %d byte(s)\n", sizeof(x));
+printf("Size of a long long int: %d byte(s)\n", sizeof(y));
+printf("Size of a float: %d byte(s)\n", sizeof(z));
+return (0);
 }
