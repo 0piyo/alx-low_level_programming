f06e5d69f780971ee9c13cfca54af65fbef054ea
erique2065 <kopiyoapps@yahoo.com>
2021-06-27 11:56:39 -0700

    nested commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _islower - checks for lowercase character
+ *
+ * @c: Character to test
+ *
+ * Return: 1 if is a lowercase letter, 0 otherwise
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_last_digit - Prints the last digit of a number
+ *
+ * @x: number to work and find the last digit
+ *
+ * Return: the value of the last digit
+ */
+int print_last_digit(int x)
+{
+if (x < 0)
+x = x * -1;
+_putchar((x % 10) + '0');
+return (x % 10);
+}
